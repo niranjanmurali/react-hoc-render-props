b824d9a84d740403a1480aaea464f77939393db0
niranjanmurali <godofgenjudsu@email.com>
2024-04-19 10:43:10 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,37 @@
 import './App.css';
-import LikeImage from './components/LikeImage';
-import LikePost from './components/LikePost';
+import LikeImage from './components/HOC/LikeImage';
+import LikePost from './components/HOC/LikePost';
+import LikeImage2 from './components/RenderProps/LikeImage2';
+import LikePost2 from './components/RenderProps/LikePost2';
+import RenderProps from './components/RenderProps/RenderProps';
 
 function App() {
   return (
-    <div>
-      <h3>Some Blog</h3>
-      <div className='buttons'>
-        <LikePost/>
-        <LikeImage/>
-      </div>
+    <div id='container' >
+    {/* HOC */}
+    <h2>Using HOC</h2>
+    <p id='blog'>Some Blog...</p>
+    <div className="buttons">
+      <LikePost />
+      <LikeImage />
     </div>
+
+    {/* Render Props */}
+    <h2>Using Render Props</h2>
+    <p id='blog'>Some Blog...</p>
+    <div className='buttons'>
+      <RenderProps
+        render={(count, handleCount) => (
+          <LikePost2 count={count} handleCount={handleCount} />
+        )}
+      />
+      <RenderProps
+        render={(count, handleCount) => (
+          <LikeImage2 count={count} handleCount={handleCount} />
+        )}
+      />
+    </div>
+  </div>
   );
 }
 
